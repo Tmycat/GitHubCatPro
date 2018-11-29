0c42778a3d39e248c5e216178c37cebc503dcc5c
Tmycat <2726238237@qq.com>
2018-11-29 09:29:39 +0800

    Initial commit

---
+++ src/main/java/cat/com/controller/DeptController.java
@@ -0,0 +1,15 @@
+package cat.com.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:王震涛
+ * createTime:2018-11-29 09:06
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("第一次提交GitHub项目！");
+    }
+
+}
